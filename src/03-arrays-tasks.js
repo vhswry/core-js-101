9e286f5cf481c1eb7cb931b4f25b259a35a50642
vhswry <vasvasa478@gmail.com>
2023-01-03 01:06:33 +0200

    feat: implemented propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -272,8 +272,15 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const res = [];
+  arr.map((value, index) => {
+    res.length += index + 1;
+    res.fill(value, res.length - index - 1);
+    return res;
+  });
+
+  return res;
 }
 
 
