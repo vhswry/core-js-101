a46651f9c1ab9445ab48faaa167583b58e377f80
vhswry <vasvasa478@gmail.com>
2023-01-03 00:42:45 +0200

    feat: implemented getSecondItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -253,8 +253,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((el, id) => id % 2);
 }
 
 
